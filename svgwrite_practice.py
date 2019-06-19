2222cd6f80921173a677993e4c7f61761926bfaa
mtassia <mgt0007@auburn.edu>
2019-06-19 14:18:34 -0500

    Successfully draws the basic structure of sequences and domains on lines

--- svgwrite_practice.py
+++ svgwrite_practice.py
@@ -3,7 +3,7 @@
 import svgwrite
 
 #Creating variables that will be needed to be imported from HMMer domain_table, will be changed to be an import function
-ANNOTATION_LIST=[["SEQ1",300,"DOMAIN_1",50,150],["SEQ1",300,"DOMAIN_2",200,268],["SEQ200",400,"DOMAIN_1",20,80],["SEQ200",400,"DOMAIN_2",81,204],["SEQ200",400,"DOMAIN_200",290,399]]
+ANNOTATION_LIST=[["SEQ1",300,"DOMAIN_1",50,150],["SEQ1",300,"DOMAIN_2",200,268],["SEQ200",400,"DOMAIN_1",20,80],["SEQ200",400,"DOMAIN_2",81,204],["SEQ200",400,"DOMAIN_200",290,399]] #Simulates a best_hit_domains.py output
 SEQUENCE_IDS=[]
 SEQUENCE_LENGTHS=[]
 SEQLEN_DICT={}
@@ -19,11 +19,12 @@ SEQUENCE_LENGTHS.sort(reverse=True) #Sort sequence lengths in descending order t
 LONGEST_SEQ=SEQUENCE_LENGTHS[0] #Use the longest sequence to generate the width of the svg canvas
 NUM_SEQS=len(SEQUENCE_IDS) #Use the number of sequences to generate the length of the svg canvas
 LONGEST_ID_LEN=len(max(SEQUENCE_IDS, key=len)) #Create an integer variable which contains the length of the longest string - this is to be used to appropriately format the width of the canvas.
+LINE_SCAFFOLD_START=(LONGEST_ID_LEN*10)+5
 
 #CREATE CANVAS
 svgdoc=svgwrite.Drawing(
     filename="test_drawing.svg",
-    size = (((LONGEST_ID_LEN*10)+LONGEST_SEQ),NUM_SEQS*30)) #At size 10 arial bold, the W glyph is 9.43px -- therefor the x for scaffold size is loaded as a function of (longest length sequence name * 10) + (length of longest sequence). Y scaffold size is loaded as the number of sequences * 30 (15 above and below scaffold line) for appropriate spacing.
+    size = (((LONGEST_ID_LEN*10)+LONGEST_SEQ+5),NUM_SEQS*30)) #At size 10 arial bold, the W glyph is 9.43px -- therefor the x for scaffold size is loaded as a function of (longest length sequence name * 10) + (length of longest sequence). Y scaffold size is loaded as the number of sequences * 30 (15 above and below scaffold line) for appropriate spacing. Final 5 accounts for the spacing between text and the line object.
 
 SEQLEN_DICT[SEQUENCE_IDS[0]] #HERE FOR TESTING PURPOSES ONLY - CAN DELETE
 #Create TEXT AND LINE-SCAFFOLD PER SEQUENCE
@@ -40,11 +41,29 @@ for SEQi in range(len(SEQLEN_DICT)):
         font_family='Arial'
     ))
     svgdoc.add(svgdoc.line( #Create the line for the sequence scaffold
-        start=(((LONGEST_ID_LEN*10)+5),((SEQi*30)+15)), #Start of the line scaffold is equal to the LONGEST_ID_LEN * 10 + 5
+        start=(LINE_SCAFFOLD_START,((SEQi*30)+15)), #Start of the line scaffold is equal to the LONGEST_ID_LEN * 10 + 5
         end=(SEQLEN_DICT[SEQUENCE_IDS[SEQi]],((SEQi*30)+15)),
         stroke='black',
         stroke_width='1px'
     ))
+
+#Iterate through annotations per sequence
+COUNT=0 #Create a count variable which will help with y-axis formatting as the annotations are looped
+for ID in SEQUENCE_IDS: #Loop through a list of the sequence IDs
+    for ANNOTATION in ANNOTATION_LIST: #Loop through entries in the annotation list
+            if ID == ANNOTATION[0]: #If the annotation line corresponds to the sequence in the list of sequences being iterated above, do the following
+                DOMAIN_NAME=ANNOTATION[2] #Load the domain name
+                DOMAIN_START=ANNOTATION[3] #Load the start coordinate for the domain
+                DOMAIN_END=ANNOTATION[4] #Load the end coordinate for the domain
+                svgdoc.add(svgdoc.rect(
+                    insert=((LINE_SCAFFOLD_START+DOMAIN_START),((COUNT*30)+7.5)),
+                    size=(((DOMAIN_END)-(DOMAIN_START)),15),
+                    rx=2, #Make rectangles with rounded edges
+                    fill="white",
+                    stroke="black",
+                    stroke_width="1px",
+                ))
+    COUNT+=1
 
 #Save Drawing
 svgdoc.saveas("test_drawing.svg")
